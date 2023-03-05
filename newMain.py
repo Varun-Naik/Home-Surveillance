0e4b8d48867eb6e159d94bddd9ea37a353786f5d
Varun N Naik <49288579+Varun-Naik@users.noreply.github.com>
2023-03-04 22:06:53 -0500

    new main code
    added options for different video dimension and extensions

--- newMain.py
+++ newMain.py
@@ -1,15 +1,18 @@
 import cv2
 
-cap= cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)
 
-width= int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-height= int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-writer= cv2.VideoWriter('basicvideo.mp4', cv2.VideoWriter_fourcc(*'DIVX'), 20, (width,height))
+#writer = cv2.VideoWriter('basicvideo.mp4', cv2.VideoWriter_fourcc('m', 'p', '4', 'v'), 30, (width, height))
+#writer = cv2.VideoWriter('basicvideo.mp4', cv2.VideoWriter_fourcc(*'DIVX'), 20, (width, height))
+writer = cv2.VideoWriter('basicvideo.avi', cv2.VideoWriter_fourcc(*'XVID'), 20, (width, height))
+
 
 
 while True:
-    ret,frame= cap.read()
+    ret, frame = cap.read()
 
     writer.write(frame)
 
@@ -17,7 +20,6 @@ while True:
 
     if cv2.waitKey(1) & 0xFF == 27:
         break
-
 
 cap.release()
 writer.release()
