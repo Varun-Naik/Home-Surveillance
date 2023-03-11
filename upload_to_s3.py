0ba789ccc59815a54654a6faca5eacc110679020
Varun N Naik <49288579+Varun-Naik@users.noreply.github.com>
2023-03-11 13:17:29 -0500

    ffmpeg code

--- upload_to_s3.py
+++ upload_to_s3.py
@@ -1,11 +1,13 @@
 import boto3
+from secretss import accessKey, secretKey
 
+# upload files to AWS S3 bucket
 s3 = boto3.client('s3',
-    aws_access_key_id='AKIA54ZHVIHWKDTM3N7M',
-    aws_secret_access_key='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')
+                  aws_access_key_id=accessKey,
+                  aws_secret_access_key=secretKey)
 
 bucket_name = 'mmc-video-bucket'
-file_path = 'E:\Programming files\Home-Surveillance\mp4_sample_video.mp4'
-object_key = 'mp4_sample_video.mp4'
+file_path = 'E:\Programming files\Home-Surveillance\output.mp4'
+object_key = 'output.mp4'
 
 s3.upload_file(file_path, bucket_name, object_key)
