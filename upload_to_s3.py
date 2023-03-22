20043fb70832434c7d417998199f6820342a5f59
Varun N Naik <49288579+Varun-Naik@users.noreply.github.com>
2023-03-21 22:53:29 -0400

    removed credentials

--- upload_to_s3.py
+++ upload_to_s3.py
@@ -2,12 +2,10 @@ import boto3
 from secretss import accessKey, secretKey
 
 # upload files to AWS S3 bucket
-s3 = boto3.client('s3',
-                  aws_access_key_id=accessKey,
-                  aws_secret_access_key=secretKey)
+s3 = boto3.client('s3')
 
-bucket_name = 'mmc-video-bucket'
-file_path = 'E:\Programming files\Home-Surveillance\output.mp4'
-object_key = 'output.mp4'
+bucket_name = "mmc-video-bucket"
+file_path = 'E:\Programming files\Home-Surveillance\\basicvideo.mp4'
+object_key = 'basicvideo.mp4'
 
 s3.upload_file(file_path, bucket_name, object_key)
